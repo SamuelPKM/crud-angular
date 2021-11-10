5e21a687bf1080dde9b1bb4ef9308fd5896c2761
SamuelPKM <samuel11299ac@gmail.com>
2021-11-10 00:35:17 -0600

    Servicio de Usuarios
    Hasta aqui todo bien, funciona el navbar del header y se crearon los componentes para el crud. Todo funciona hasta aqui. 18:48

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'list', loadChildren: () => import('./pages/Usuarios/list/list.module').then(m => m.ListModule) }, { path: 'details', loadChildren: () => import('./pages/Usuarios/details/details.module').then(m => m.DetailsModule) }, { path: 'edit', loadChildren: () => import('./pages/Usuarios/edit/edit.module').then(m => m.EditModule) }, { path: 'new', loadChildren: () => import('./pages/Usuarios/new/new.module').then(m => m.NewModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
